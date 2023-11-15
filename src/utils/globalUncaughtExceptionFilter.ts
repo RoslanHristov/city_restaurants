1b04e20e7847efc57b250193bcad0e1592ce87b4
Roslan Hristov <roslan.hristov@rs-components.com>
2023-11-16 00:25:39 +0200

    updated global error handling pipe

--- src/utils/globalUncaughtExceptionFilter.ts
+++ src/utils/globalUncaughtExceptionFilter.ts
@@ -1,14 +1,29 @@
-import { Catch, ExceptionFilter } from "@nestjs/common";
+import {
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: any) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
+
     if (!exception.response) {
+      const statusCode =
+        exception instanceof HttpException
+          ? exception.getStatus()
+          : HttpStatus.INTERNAL_SERVER_ERROR;
+
+      const errMessage =
+        exception instanceof HttpException
+          ? exception.message || exception.message?.["error"]
+          : "Internal server error";
+
       response.status(500).json({
-        statusCode: 500,
-        message: `Whoops something went wrong, but it's fine :) !`,
-        exception: `(DEV PURPUSE LOGGING) ${exception} `,
+        statusCode,
+        message: `Whoops something went wrong, but it's fine :) ! \n ${errMessage}`,
       });
     } else {
       response.status(exception.getStatus()).json({
